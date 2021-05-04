1744f77a187ba10003dd1fc07ac8ca85d191e558
Brutos212 <brutos212@gmail.com>
2021-05-04 17:09:05 -0300

    Project Created

---
+++ backend/src/test/java/com/devestudando/devendas/DevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devestudando.devendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
