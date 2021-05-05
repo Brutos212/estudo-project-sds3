d2196253ee2f485394def0b2a7347e27a9147e8e
Brutos212 <brutos212@gmail.com>
2021-05-05 17:45:48 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Brutos212" target="_blank" rel="noreferrer">Fabio Santos</a></p>
+                <p className="text-light"><small><strong>Semana de estudo com Spring e  React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    )
+
+}
+
+export default Footer;
